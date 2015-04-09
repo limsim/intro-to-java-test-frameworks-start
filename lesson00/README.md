78147d8605ad1fb11ad358513243b1ff9c610d5f
Lim Sim <lsim@equalexperts.com>
2015-04-09 17:26:49 +0100

    initial commit

---
+++ lesson00/README.md
@@ -0,0 +1,4 @@
+# Lesson 0
+
+* This is a base project that has a basic calculator program that can sum up two integers.
+* Try out the maven command to run test: ``` mvn test```
---
+++ lesson00/src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+/**
+ * Created by limsim on 23/03/15.
+ */
+public class Calculator {
+
+    private int result;
+
+    public int add(int firstNumber, int secondNumber) {
+        result = firstNumber + secondNumber;
+        return result;
+    }
+}
